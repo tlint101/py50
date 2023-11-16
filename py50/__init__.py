9f10e273e302d5227b3261a708436bf7694f86ea
tlint <tonyelin@gmail.com>
2023-11-16 11:54:13 +0800

    Updated __init__.py

---
+++ py50/__init__.py
@@ -0,0 +1,14 @@
+"""
+============
+py50
+============
+
+Calculate and draw IC50 curves.
+
+This package will import the following:
+
+"""
+__url__ = 'https://github.com/tlint101/py50'
+
+from py50.calculate import Calculate
+from py50.plotcurve import PlotCurve
