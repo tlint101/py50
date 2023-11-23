f563b2c3fbc9ac6e15988754ccd8833afedffc7c
tlint <tonyelin@gmail.com>
2023-11-23 11:08:47 +0800

    Update documents in docs directory

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,7 @@ release = 'v0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_rtd_theme',
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
