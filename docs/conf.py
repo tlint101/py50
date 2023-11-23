75d4c2f233cf784bd0d5c6c94b996dcb7f92b7a7
tlint <tonyelin@gmail.com>
2023-11-23 11:07:37 +0800

    Update documents in docs directory

--- docs/conf.py
+++ docs/conf.py
@@ -14,14 +14,17 @@ release = 'v0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme',]
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
+html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,9 +1,10 @@
 .. py50 documentation master file, created by
-   sphinx-quickstart on Thu Nov 23 10:24:52 2023.
+   sphinx-quickstart on Thu Nov 23 10:51:50 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
 Welcome to py50's documentation!
+The module
 ================================
 
 .. toctree::
