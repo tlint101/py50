646f8cbf5b13afd1a65817119907e89f1e674184
tlint <tonyelin@gmail.com>
2023-11-27 23:42:21 +0800

    Update streamlit welcome.py

--- py50-streamlit/Welcome.py
+++ py50-streamlit/Welcome.py
@@ -1,11 +1,16 @@
 import streamlit as st
+from streamlit_extras.app_logo import add_logo
+from PIL import Image
 
 st.set_page_config(
     page_title="Welcome",
     page_icon="👋",
 )
 
-st.write("# Welcome to Py50: Generate Dose-Response Curves")  # add a title
+# Place logo image at top of page
+image = Image.open('img/py50_full.png')
+st.image(image)
+st.write('# Welcome to Py50!')  # add a title
 
 st.markdown(
     """
@@ -13,4 +18,7 @@ st.markdown(
 """
 )
 
+
+# add logo
+st.sidebar.image('img/py50_logo_only.png', width=150)
 st.sidebar.success("Select page above to get started!")
