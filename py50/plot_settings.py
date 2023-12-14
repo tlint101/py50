3c1b589c0c848e80ecc3268eac7c62aabd9a4972
tlint <tonyelin@gmail.com>
2023-12-14 12:31:18 +0800

    update plot settings. Included options for concentration input.

--- py50/plot_settings.py
+++ py50/plot_settings.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import interp1d
+from scipy.optimize import curve_fit
 
 """
 Color and Marker schemes and functions for plotting
@@ -16,49 +17,66 @@ CBPALETTE = ('#000000', '#E69F00', '#56B4E9', '#009E73',
 CBMARKERS = ('o', '^', 's', 'D', 'v', '<', '>', 'p')
 assert len(CBPALETTE) == len(CBMARKERS)
 
-
+# todo fill out param documentation
 class CurveSettings:
-    def scale_units(self, xscale_unit, xscale_ticks): # todo have xscale_unit be automatic based on concentration?
-        """Logic function for curve plot"""
+    def scale_units(self, xscale_unit, xscale_ticks, verbose=None):
+        """
+        Logic function for curve plot
 
-        if xscale_unit == None and xscale_ticks == None:
-            x_fit = np.logspace(0, 5, 100)
+        :param xscale_unit:
+        :param xscale_ticks:
+        :param verbose:
+        """
+
+        if xscale_unit == 'nM' and xscale_ticks is None:
+            x_fit = np.logspace(0, 1, 1000)
+            if verbose is True:
+                print(f'Input concentration in {xscale_unit}!')
             return x_fit
-        elif xscale_unit == 'nM' and xscale_ticks == None:
-            x_fit = np.logspace(0, 5, 100)
-            print('ticks constrained to nM!')
+        elif xscale_unit == 'µM' or xscale_unit == 'uM' and xscale_ticks is None:
+            x_fit = np.logspace(-3, 3, 1000)
+            if verbose is True:
+                print(f'Input concentration in {xscale_unit}!')
             return x_fit
-        elif xscale_unit == 'µM' and xscale_ticks == None:
-            print('ticks constrained to µM!')
-            x_fit = np.logspace(-3, 3, 100)
-            return x_fit
-        elif xscale_unit == 'µM' and xscale_ticks is not None:
-            print('ticks constrained to µM!')
-            x_fit = np.logspace(xscale_ticks[0], xscale_ticks[1], 100)
+        elif xscale_unit == 'µM' or xscale_unit == 'uM' and xscale_ticks is not None:
+            x_fit = np.logspace(xscale_ticks[0], xscale_ticks[1], 1000)
+            if verbose is True:
+                print(f'Input concentration in {xscale_unit}!')
             return x_fit
         elif xscale_unit == 'nM' and xscale_ticks is not None:
-            print('ticks constrained to nM!')
-            x_fit = np.logspace(xscale_ticks[0], xscale_ticks[1], 100)
+            x_fit = np.logspace(xscale_ticks[0], xscale_ticks[1], 1000)
+            if verbose is True:
+                print(f'Input concentration in {xscale_unit}!')
             return x_fit
-        elif xscale_unit == 'uM' and xscale_ticks is not None:
-            print('ticks constrained to µM!')
-            x_fit = np.logspace(xscale_ticks[0], xscale_ticks[1], 100)
-            return x_fit
-        else:
-            print('Insufficient input for xscale_unit and xscale_ticks')
 
-    def xscale(self, xscale_unit, concentration):
-        """Logic function for curve plot"""
+        elif xscale_unit == None and xscale_ticks == None:
+            xscale_unit == 'nM'
+            x_fit = np.logspace(0, 5, 1000)
+            if verbose is True:
+                print(f'Assuming input concentration are in {xscale_unit}!')
+            return x_fit
+
+
+    def xscale(self, xscale_unit, concentration, verbose=None):
+        """
+        Logic function for curve plot
+        :param xscale_unit:
+        :param concentration:
+        :param verbose:
+        """
 
         if xscale_unit == 'nM':
-            print('Concentration on X-axis will be nM')
+            if verbose is True:
+                print('Concentration on X-axis will be nM')
             return concentration
         elif xscale_unit == 'uM':
-            print('Concentration on X-axis will be µM')
+            if verbose is True:
+                print('Concentration on X-axis will be µM')
             concentration = concentration / 1000  # convert drug concentration to µM
             return concentration
         elif xscale_unit == 'µM':
-            print('Concentration on X-axis will be µM')
+            if verbose is True:
+                print('Concentration on X-axis will be µM')
             concentration = concentration / 1000  # convert drug concentration to µM
             return concentration
         else:
@@ -83,11 +101,11 @@ class CurveSettings:
     def multi_curve_box_highlight(self, box_target=None, box_color=None, box_intercept=None,
                                   y_intersection=None, x_intersection=None, name_list=None, y_fit=None, y_fit_list=None,
                                   x_fit=None, reverse=None, ymin=0, ymax_vline=50):
-        if box_target is True and reverse is 1:
+        if box_target is True and reverse == 1:
             y_intersection = box_intercept
             interpretation = interp1d(y_fit, x_fit, kind='linear', fill_value="extrapolate")
             x_intersection = interpretation(y_intersection)
-        elif box_target is True and reverse is 0:
+        elif box_target is True and reverse == 0:
             ymin = 0  # Starts at the bottom of the plot
             ymax = (y_intersection - plt.gca().get_ylim()[0]) / (plt.gca().get_ylim()[1] - plt.gca().get_ylim()[0])
             # Converted x_intersection from a numpy array into a float
@@ -105,7 +123,7 @@ class CurveSettings:
                     x_intersection = np.interp(y_intersection, y_fit_list[0], x_fit)
                     ymin = 0  # Starts at the bottom of the plot
                     ymax_vline = (y_intersection - plt.gca().get_ylim()[0]) / (
-                                plt.gca().get_ylim()[1] - plt.gca().get_ylim()[0])
+                            plt.gca().get_ylim()[1] - plt.gca().get_ylim()[0])
                 # Converted x_intersection from a numpy array into a float
                 plt.axvline(x=x_intersection.item(), ymin=ymin, ymax=ymax_vline, color=box_color, linestyle='--')
                 plt.hlines(y=y_intersection, xmin=0, xmax=x_intersection, colors=box_color, linestyles='--')
@@ -113,6 +131,7 @@ class CurveSettings:
                 print('Drug name does not match box target!')
         else:
             pass
+
 
 if __name__ == '__main__':
     import doctest
