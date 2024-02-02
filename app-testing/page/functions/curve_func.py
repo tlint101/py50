2e15b05ab1bfb715c1362b5afe41fa7f752d5b99
Tony Lin <tonyelin@gmail.com>
2024-02-02 09:36:33 +0800

    Refactor code with Black

--- app-testing/page/functions/curve_func.py
+++ app-testing/page/functions/curve_func.py
@@ -56,38 +56,43 @@ class Plot_Logic:
             }
             return options
 
+    def label_options_true(label_options):
+        """
+        Function to for label_options input
+        """
+        plot_title = st.sidebar.text_input(
+            label="Plot Title", placeholder="Input Plot Title"
+        )
+        font = st.sidebar.text_input(
+            label="Figure Font", value=None, placeholder="DejaVu Sans"
+        )
+        plot_title_size = st.sidebar.number_input(
+            label="Title Size", value=None, placeholder="16"
+        )
+        xlabel = st.sidebar.text_input(
+            label="X-Axis Label (Default in µM)", placeholder="Input X Label"
+        )
+        ylabel = st.sidebar.text_input(
+            label="Y-Axis Label", placeholder="Input Y Label"
+        )
+        axis_label_fontsize = st.sidebar.number_input(
+            label="Axis Fontsize", value=None, placeholder="14"
+        )
+        ymax = st.sidebar.number_input(
+            label="Y-Axis Max", value=None, placeholder="Y Max"
+        )
+        ymin = st.sidebar.number_input(
+            label="Y-Axis Min", value=None, placeholder="Y Min"
+        )
 
-def label_options_true(label_options):
-    """
-    Function to for label_options input
-    """
-    plot_title = st.sidebar.text_input(
-        label="Plot Title", placeholder="Input Plot Title"
-    )
-    font = st.sidebar.text_input(
-        label="Figure Font", value=None, placeholder="DejaVu Sans"
-    )
-    plot_title_size = st.sidebar.number_input(
-        label="Title Size", value=None, placeholder="16"
-    )
-    xlabel = st.sidebar.text_input(
-        label="X-Axis Label (Default in µM)", placeholder="Input X Label"
-    )
-    ylabel = st.sidebar.text_input(label="Y-Axis Label", placeholder="Input Y Label")
-    axis_label_fontsize = st.sidebar.number_input(
-        label="Axis Fontsize", value=None, placeholder="14"
-    )
-    ymax = st.sidebar.number_input(label="Y-Axis Max", value=None, placeholder="Y Max")
-    ymin = st.sidebar.number_input(label="Y-Axis Min", value=None, placeholder="Y Min")
-
-    return (
-        label_options,
-        plot_title_size,
-        plot_title,
-        font,
-        axis_label_fontsize,
-        xlabel,
-        ylabel,
-        ymax,
-        ymin,
-    )
+        return (
+            label_options,
+            plot_title_size,
+            plot_title,
+            font,
+            axis_label_fontsize,
+            xlabel,
+            ylabel,
+            ymax,
+            ymin,
+        )
