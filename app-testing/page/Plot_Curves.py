319db8a490ce4a667ff10a6e7aa8867b9dc72b0d
Tony Lin <tonyelin@gmail.com>
2024-02-02 09:37:00 +0800

    Removed commended code from previous version

--- app-testing/page/Plot_Curves.py
+++ app-testing/page/Plot_Curves.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from py50.plotcurve import PlotCurve
 from py50.plot_settings import CBMARKERS, CBPALETTE
-from page.functions.plot_functions import Plot_Logic
+from page.functions.curve_func import Plot_Logic
 
 # todo convert functions into class and instantiate
 
@@ -24,66 +24,6 @@ background-color: transparent;
 }
 """
 st.markdown(links, unsafe_allow_html=True)
-
-
-# # housekeeping functions
-# def label_plot_options(label_options, plot_title_size, plot_title, font, axis_fontsize, xlabel, ylabel, ymax, ymin):
-#     """
-#     Function to organize plat label  options
-#     """
-#     if label_options:
-#         # set the font name for a font family
-#         if font is None:
-#             plt.rcParams.update({'font.sans-serif': 'DejaVu Sans'})
-#         else:
-#             plt.rcParams.update({'font.sans-serif': font})
-#
-#         # Default label sizes
-#         plot_title_size = plot_title_size if plot_title_size else 16
-#         axis_fontsize = axis_fontsize if axis_fontsize else 14
-#
-#         # Y axis limit
-#         ymax = int(ymax) if ymax is not None else None
-#         ymin = int(ymin) if ymin is not None else None
-#
-#         # Return options
-#         options = {
-#             'plot_title': plot_title,
-#             'plot_title_size': plot_title_size,
-#             'axis_fontsize': axis_fontsize,
-#             'xlabel': xlabel,
-#             'ylabel': ylabel,
-#             'ymax': ymax,
-#             'ymin': ymin
-#         }
-#         return options
-#     else:
-#         options = {
-#             'plot_title': None,
-#             'plot_title_size': 16,
-#             'axis_fontsize': 14,
-#             'xlabel': '',
-#             'ylabel': '',
-#             'ymax': None,
-#             'ymin': None
-#         }
-#         return options
-
-
-# def label_options_true(label_options):
-#     """
-#     Function to for label_options input
-#     """
-#     plot_title = st.sidebar.text_input(label='Plot Title', placeholder='Input Plot Title')
-#     font = st.sidebar.text_input(label='Figure Font', value=None, placeholder='DejaVu Sans')
-#     plot_title_size = st.sidebar.number_input(label='Title Size', value=None, placeholder='16')
-#     xlabel = st.sidebar.text_input(label='X-Axis Label (Default in µM)', placeholder='Input X Label')
-#     ylabel = st.sidebar.text_input(label='Y-Axis Label', placeholder='Input Y Label')
-#     axis_label_fontsize = st.sidebar.number_input(label='Axis Fontsize', value=None, placeholder='14')
-#     ymax = st.sidebar.number_input(label="Y-Axis Max", value=None, placeholder='Y Max')
-#     ymin = st.sidebar.number_input(label="Y-Axis Min", value=None, placeholder='Y Min')
-#
-#     return label_options, plot_title_size, plot_title, font, axis_label_fontsize, xlabel, ylabel, ymax, ymin
 
 
 def line_options(line_color_option, line_color, line_width, marker):
@@ -331,7 +271,7 @@ def plot_program(df=None, paste=True):
                 ylabel,
                 ymax,
                 ymin,
-            ) = label_options_true(label_options=label_options)
+            ) = Plot_Logic.label_options_true(label_options=label_options)
 
             # logic based on plot options above
             Plot_Logic.label_plot_options(
@@ -536,7 +476,7 @@ def plot_program(df=None, paste=True):
                 ylabel,
                 ymax,
                 ymin,
-            ) = label_options_true(label_options=label_options)
+            ) = Plot_Logic.label_options_true(label_options=label_options)
 
             # logic based on plot options above
             Plot_Logic.label_plot_options(
@@ -753,10 +693,10 @@ def plot_program(df=None, paste=True):
                 ylabel,
                 ymax,
                 ymin,
-            ) = label_options_true(label_options=label_options)
+            ) = Plot_Logic.label_options_true(label_options=label_options)
 
             # logic based on plot options above
-            Plot_Logic_label_plot_options(
+            Plot_Logic.label_plot_options(
                 label_options,
                 plot_title_size,
                 plot_title,
