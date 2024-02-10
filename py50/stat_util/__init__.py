9e14ca822deae2d2417c2785ad73d655f02e790d
Tony Lin <tonyelin@gmail.com>
2024-02-10 23:32:19 +0800

    Added color palette option for plots

--- py50/stat_util/__init__.py
+++ py50/stat_util/__init__.py
@@ -2,15 +2,58 @@
 The following script holds plot logic for the indicated tests. This was created to reduce the "look" of the stats file
 for maintainability.
 """
-import pandas as pd
+import seaborn as sns
+
+def star_value(p_value):
+    """
+    Scrip to convert p values from tables into traditional star (*) format for plotting.
+    :param p_value: A list of p-values obtained from a DataFrame.
+    :return: A list containing the corresponding star (*) or "n.s." string.
+    """
+
+    if p_value < 0.0001:
+        return "****"
+    elif p_value < 0.001:
+        return "***"
+    elif p_value < 0.01:
+        return "**"
+    elif p_value < 0.05:
+        return "*"
+    else:
+        return "n.s."
 
 
-def tukey_plot_logic(stat, test_value):
+def palette(list=None):
+    if list:
+        colors = sns.color_palette(list)
+    else:
+        colors = sns.color_palette()
+    return colors
+
+
+def tukey_plot_logic(test_value):
     """
 
     :param stat:
     :param test_value:
     :return:
     """
-    pvalue = [stat.star_value(value) for value in test_value["p-tukey"].tolist()]
+    pvalue = [star_value(value) for value in test_value["p-tukey"].tolist()]
     return pvalue
+
+
+def gameshowell_plot_logic(test_value):
+    """
+
+    :param stat:
+    :param test_value:
+    :return:
+    """
+    pvalue = [star_value(value) for value in test_value["pval"].tolist()]
+    return pvalue
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
