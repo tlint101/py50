2d9f0eb2768742bcbc7586177e74fbef74cb4a3b
Tony Lin <tonyelin@gmail.com>
2024-02-12 15:56:25 +0800

    update py50 init file

--- py50/__init__.py
+++ py50/__init__.py
@@ -5,11 +5,5 @@ py50
 
 Calculate and draw IC50 curves.
 
-This package will import the following:
-
 """
 
-__url__ = "https://github.com/tlint101/py50"
-
-from py50.calculator import Calculator
-from py50.plotcurve import PlotCurve
