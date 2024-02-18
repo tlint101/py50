1355e4b7438cdf1b0437f0daff2178e870259f49
Tony Lin <tonyelin@gmail.com>
2024-02-18 13:03:46 +0800

    Added logic for p_matrix

--- py50/utils.py
+++ py50/utils.py
@@ -4,6 +4,7 @@ for maintainability.
 """
 
 from itertools import combinations
+import pandas as pd
 import seaborn as sns
 
 
@@ -35,7 +36,7 @@ def palette(list=None):
 
 
 # todo may remove
-def get_pairs(df, group_col=None, value_col=None, pairs = 2):
+def get_pairs(df, group_col=None, value_col=None, pairs=2):
     """Input DataFrame and generate pairs for each category column"""
 
     # Generate pairs of categories
@@ -70,6 +71,61 @@ def gameshowell_plot_logic(test_value):
     """
     pvalue = [star_value(value) for value in test_value["pval"].tolist()]
     return pvalue
+
+
+def multi_group(df, group_col1=None, group_col2=None, test=None):
+    """
+    Logic for obtaining p matrix. This is for tests outputs a multiple column with categorical data.
+    :param df:
+    :param kwargs:
+    :param test:
+    :param x_axis:
+    :param y_axis:
+    :return:
+    """
+    global p_col
+    if test == 'tukey':
+        p_col = "p-tukey"
+    groups = sorted(set(df[group_col1]) | set(df[group_col2]))
+    matrix_df = pd.DataFrame(index=groups, columns=groups)
+
+    # Fill the matrix with p-values
+    for i, row in df.iterrows():
+        matrix_df.loc[row[group_col1], row[group_col2]] = row[p_col]
+        matrix_df.loc[row[group_col2], row[group_col1]] = row[p_col]
+
+    # Fill NaN cells with NS (Not Significant)
+    matrix_df.fillna(1, inplace=True)
+
+    return matrix_df
+
+
+def single_group(df, group_col=None, test=None):
+    """
+    Logic for obtaining p matrix. This is for tests that only outputs a single column with categorical data.
+    :param df:
+    :param group_col:
+    :param p_col:
+    :return:
+    """
+    if test is None:
+        p_col = "p-val"
+    else:
+        p_col = "p-val"
+    # Get unique groups
+    groups = sorted(set(df[group_col]))
+
+    # Create an empty DataFrame with groups as both index and columns
+    matrix_df = pd.DataFrame(index=groups, columns=groups)
+
+    # Fill the matrix with values
+    for i, row in df.iterrows():
+        matrix_df.loc[row[group_col], row[group_col]] = row[p_col]
+
+    # Fill NaN cells if any
+    matrix_df.fillna(1, inplace=True)
+
+    return matrix_df
 
 
 if __name__ == "__main__":
