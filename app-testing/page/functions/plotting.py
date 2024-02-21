470e7e60dc6d0e164cbe122216fd0a606aaf4a73
Tony Lin <tonyelin@gmail.com>
2024-02-21 11:33:11 +0800

    Start stat and plot functions

---
+++ app-testing/page/functions/plotting.py
@@ -0,0 +1,11 @@
+"""
+functions for plotting
+"""
+
+import streamlit as st
+import io
+from matplotlib import pyplot as plt
+import seaborn as sns
+from py50.stats import Plots
+
+plots = Plots()
---
+++ app-testing/page/functions/stats.py
@@ -0,0 +1,9 @@
+"""
+Functions for statistic calculations
+"""
+
+import streamlit as st
+import io
+from py50.stats import Stats
+
+stats = Stats()
